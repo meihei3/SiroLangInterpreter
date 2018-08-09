ec8bb5e64ce5d0fda7cc458808a35b27f053aeb6
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-08-10 01:06:02 +0900

    Add Command class and replace string comparison with class

--- sbf.js
+++ sbf.js
@@ -1,3 +1,20 @@
+// enum commands
+class Command {
+    constructor(num, name) {
+	this.num = num;
+	this.name = name;
+    }
+}
+const inc_ptr = new Command(0, 'いーねっ！');
+const dec_ptr = new Command(1, 'ｷｭｰｲ');
+const inc_val = new Command(2, 'おほほい');
+const dec_val = new Command(3, 'ぱいーん');
+const begin_loop = new Command(4, '白組さん');
+const end_loop = new Command(5, '救済');
+const std_input = new Command(6, 'ズンドコズンドコ♪');
+const std_output = new Command(7, 'なんて日だ！');
+const all_commands = [inc_ptr, dec_ptr, inc_val, dec_val, begin_loop, end_loop, std_input, std_output];
+
 function run(){
     const doc = document.getElementById('source').value;
     document.getElementById('out').innerHTML = fuck(doc);
@@ -20,10 +37,13 @@ function fuck(docstr){
     	let c = 1;
     	while(ptr < cmdSize && c > 0){
     	    ptr++;
-    	    if(commands[ptr] === '白組さん')
-    	        c++;
-    	    if(commands[ptr] === '救済')
-    	        c--;
+	    const command = commands[ptr];
+    	    if (Object.is(command, begin_loop)) {
+		c++;
+	    }
+	    if (Object.is(command, end_loop)) {
+		c--;
+	    }
     	}
     	return ptr - 1;
     }
@@ -32,16 +52,19 @@ function fuck(docstr){
     	let c = 1;
     	while(ptr < cmdSize && c > 0){
     	    ptr--;
-    	    if(commands[ptr] === '救済')
-    	        c++;
-    	    if(commands[ptr] === '白組さん')
-    	        c--;
+	    const command = commands[ptr];
+	    if (Object.is(command, end_loop)) {
+		c++;
+	    }
+	    if (Object.is(command, begin_loop)) {
+		c--;
+	    }
     	}
     	return ptr - 1;
     }
 
-    const limit = 10000
-    let cnt = 0
+    const limit = 10000;
+    let cnt = 0;
 
     const startTime = Date.now();
     const maxConputationalTime = 1000; // unit: milliseconds
@@ -55,47 +78,51 @@ function fuck(docstr){
         }
         cnt++;
 
-        switch(commands[cmdptr]) {
-        case "おほほい":
-            cells[cellptr]++;
-            break;
-
-        case "ぱいーん":
-            cells[cellptr]--;
-            break;
-
-        case "いーねっ！":
-            cellptr++;
-            break;
-
-        case "ｷｭｰｲ":
-            cellptr--;
-            break;
-
-        case "なんて日だ！":
-            output += String.fromCharCode(cells[cellptr]);
-            break;
-
-        case "白組さん":
-            if (cells[cellptr] === 0)
-                cmdptr = jumpRight(cmdptr);
-            break;
-
-        case "救済":
-            if (cells[cellptr] !== 0)
-                cmdptr = jumpLeft(cmdptr);
-            break;
-
-        default:
-            break;
-        }
+	const command = commands[cmdptr];
+	if (Object.is(command, inc_val)) {
+	    cells[cellptr]++;
+	} else if (Object.is(command, dec_val)) {
+	    cells[cellptr]--;
+	} else if (Object.is(command, inc_ptr)) {
+	    cellptr++;
+	} else if (Object.is(command, dec_ptr)) {
+	    cellptr--;
+	} else if (Object.is(command, begin_loop)) {
+	    if (cells[cellptr] === 0) {
+		cmdptr = jumpRight(cmdptr);
+	    }
+	} else if (Object.is(command, end_loop)) {
+	    if (cells[cellptr] !== 0) {
+		cmdptr = jumpLeft(cmdptr);
+	    }
+	} else if (Object.is(command, std_output)) {
+	    output += String.fromCharCode(cells[cellptr]);
+	} else if (Object.is(command, std_input)) {
+	    return `Sorry, the command ${ command.name } is currently not supported. Thanks!`;
+	}
     }
 
     return output;
 }
 
 function cleanUp(src){
-    const result = src.match(/(いーねっ！|おほほい|ｷｭｰｲ|ぱいーん|白組さん|救済|なんて日だ！|ズンドコズンドコ♪)/g);
+    const all_names = all_commands.map((command) => {return command.name});
+    const pattern = RegExp(`(${ all_names.join('|') })`, 'g');
+    const resultWithString = src.match(pattern);
+    if (resultWithString === null) {
+	return null;
+    }
+    // seems little bit ugly, but the computation cost is up to O(NM)
+    // where N equals to # of commands and M equals to # of kind of commands
+    function getCorrespondingCommand(name) {
+	for (const command of all_commands) {
+	    if (name === command.name) {
+		return command;
+	    }
+	}
+	throw `ValueError: No such command ${ name }`;
+    }
+    const result = resultWithString.map(getCorrespondingCommand);
     return result;
 }
 
